323b719db30a3b7c8afb5bd4d4463f882ecf5ce9
Diego <me@diegor.nl>
2021-08-28 17:02:52 +0200

    Install and configure TailwindCSS

---
+++ webpack.mix.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+const mix = require("laravel-mix");
+const tailwindcss = require("tailwindcss");
+require("laravel-mix-versionhash");
+
+mix.js(["resources/js/app.js"], "web/assets/js/app.js").extract();
+
+mix
+    .sass("resources/sass/app.scss", "assets/css/app.css")
+    .setPublicPath("web")
+    .options({
+        processCssUrls: false,
+        postCss: [tailwindcss("./tailwind.config.js")]
+    });
+
+// Run BrowserSync Locally, off by default
+if (!mix.inProduction()) {
+	mix.browserSync({
+		// Set this to a variable in your .env file containing your local development URL:
+		proxy: process.env.PRIMARY_SITE_URL,
+		// Watch for any changes in assets/ and templates/ directories:
+		files: ['assets/**/*', 'templates/**/*'],
+	});
+}
+
+// Only run in Production
+if (mix.inProduction()) {
+    mix.versionHash();
+}

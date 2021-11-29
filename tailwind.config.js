8f323bd90ac439eb6e1466649616be6417fd8409
Diego <me@diegor.nl>
2021-11-29 04:20:22 +0100

    Changelog so big I don't even know how to name it
    I went a bit crazy ok

--- tailwind.config.js
+++ tailwind.config.js
@@ -137,6 +137,39 @@ module.exports = {
           900: "#751a3d",
         },
       },
+      typography: {
+        DEFAULT: {
+          css: {
+            strong: {
+              color: 'inherit',
+            },
+            b: {
+              color: 'inherit',
+            },
+            h1: {
+              color: 'inherit',
+            },
+            h2: {
+              color: 'inherit',
+            },
+            h3: {
+              color: 'inherit',
+            },
+            h4: {
+              color: 'inherit',
+            },
+            h5: {
+              color: 'inherit',
+            },
+            h6: {
+              color: 'inherit',
+            },
+            code: {
+              color: 'inherit',
+            },
+          }
+        }
+      }
     },
   },
   variants: {},
